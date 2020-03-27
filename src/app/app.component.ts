c04772a6506a1482668c34d477a66d72625926dd
minhladung051120 <dunglvph07397@fpt.edu.vn>
2020-03-27 23:13:29 +0700

    4 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.css',
+              './css/global.css',
+              './css/style.css',
+              './css/easy-responsive-tabs.css' ]
 })
 export class AppComponent  {
   name = 'Angular';
--- src/app/slide/slide.component.ts
+++ src/app/slide/slide.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-slide',
   templateUrl: './slide.component.html',
-  styleUrls: ['./slide.component.css']
+  styleUrls: ['./slide.component.css',
+              '../css/global.css',
+              '../css/style.css',
+              '../css/easy-responsive-tabs.css']
 })
 export class SlideComponent implements OnInit {
 
