6d9f5e30f0e9e25f454a6ae88f16db6900f35b50
minhladung051120 <dunglvph07397@fpt.edu.vn>
2020-03-27 21:17:16 +0700

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
+import { ProductListComponent } from "./product-list/product-list.component";
+import { ProductManagerComponent } from "./product-manager/product-manager.component";
+
+
+
+
+const routes: Routes = [
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", component: HomeComponent },
+
+  { path: "product", component: ProductListComponent },
+  { path: "product/manager", component: ProductManagerComponent },
+  { path: "404", component: NotFoundComponent },
+  { path: "**", redirectTo: "404", pathMatch: "full" }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
